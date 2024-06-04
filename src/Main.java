defbf28ed147121237dd48407cd70dc4bf7d442b
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-06-04 15:40:54 +0700

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import singleton.Singleton;
+import factory.Shape;
+import factory.ShapeFactory;
 
 public class Main {
     public static void main(String[] args) {
 //        Singleton singleton = Singleton.getInstance();
 //        singleton.showMessage();
 
+        ShapeFactory shapeFactory = new ShapeFactory();
 
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        shape1.draw();
     }
 }
---
+++ src/observer/BinaryObserver.java
@@ -0,0 +1,11 @@
+package observer;
+
+public class BinaryObserver extends Observer {
+    public BinaryObserver(Subject subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+    public void update() {
+        System.out.println("Binary String: " + Integer.toBinaryString(subject.getState()));
+    }
+}
