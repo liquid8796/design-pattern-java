33cb0137fd95bbe4fac52fee6eb33019c1b1e364
liquid8796 <hanam.tranle.5@gmail.com>
2024-10-16 03:31:46 +0000

    2024-10-16 10:31:46 +07:00 Asia/Bangkok

---
+++ src/proxy/ProxyPatternDemo.java
@@ -0,0 +1,14 @@
+package proxy;
+
+public class ProxyPatternDemo {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_image.jpg");
+
+        // Image will be loaded from disk
+        image.display();
+        System.out.println("");
+
+        // Image will not be loaded again from disk
+        image.display();
+    }
+}
---
+++ src/proxy/RealImage.java
@@ -0,0 +1,20 @@
+package proxy;
+
+// RealSubject class that is expensive to create (for example, loading an image from disk)
+public class RealImage implements Image{
+    private String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk();
+    }
+
+    private void loadFromDisk() {
+        System.out.println("Loading " + fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+}
